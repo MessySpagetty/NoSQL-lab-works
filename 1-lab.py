7c3398261da29c38eff09bd64689b0d41d0aaf0b
MessySpagetty <115343310+MessySpagetty@users.noreply.github.com>
2024-11-03 17:24:09 +0300

    Минимальный рабочий пример подсключения к Redis

--- 1-lab.py
+++ 1-lab.py
@@ -1,12 +1,20 @@
 import redis
 
-# Connect to Redis
-client = redis.StrictRedis(host='localhost', port=6379, db=0)
+with open('host', 'r') as file:
+    HOST = file.read()  # Reads the entire file
+
+with open('passwd', 'r') as file:
+    PASSWORD = file.read()
+
+client = redis.StrictRedis(host=HOST, password=PASSWORD)
+
+# Префикс для уникальности ключей, чтобы совместно работать в БД
+my_prefix = "poskitt_22304_"
 
 # Set a key-value pair
-client.set('my_key', 'Hello, Redis!')
+client.set(my_prefix + 'my_key', 'Hello, Redis!')
 
 # Retrieve the value
-value = client.get('my_key')
+value = client.get(my_prefix + 'my_key')
 
 print(value.decode('utf-8'))  # Output: Hello, Redis!
