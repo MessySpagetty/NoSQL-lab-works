1afb52bbdf8305f82f20185d4d683ba80f04949d
MessySpagetty <115343310+MessySpagetty@users.noreply.github.com>
2024-11-03 19:24:27 +0300

    Реализованы чекбоксы для выбора начертания шрифта. Нужно подумать над обработчиками

--- 1-lab.py
+++ 1-lab.py
@@ -26,11 +26,37 @@ choose_user_combo.pack()
 settings_title_lbl = tk.Label(root, text="Задайте настройки для текущего пользователя:")
 settings_title_lbl.pack()
 
-settings_font_lbl = tk.Label(root, text="Шрифт:")
-settings_title_lbl.pack()
+settings_font_name_lbl = tk.Label(root, text="Шрифт:")
+settings_font_name_lbl.pack()
+settings_font_name_combo = ttk.Combobox(root, values=list(font.families()))
+settings_font_name_combo.pack()
 
-settings_font_combo = ttk.Combobox(root, values=list(font.families()))
-settings_font_combo.pack()
+settings_font_size_lbl = tk.Label(root, text="Размер шрифта:")
+settings_font_size_lbl.pack()
+settings_font_size_entr = tk.Entry(root)
+settings_font_size_entr.pack()
+
+# Настройки начертания текста
+settings_font_style_lbl = tk.Label(root, text="Начертание:")
+settings_font_style_lbl.pack()
+
+bold_var = tk.BooleanVar()
+settings_font_bold_checkbox = tk.Checkbutton(root, text="Bold", variable=bold_var)
+italic_var = tk.BooleanVar()
+settings_font_italic_checkbox = tk.Checkbutton(root, text="Italic", variable=italic_var)
+underline_var = tk.BooleanVar()
+settings_font_underline_checkbox = tk.Checkbutton(root, text="Underline", variable=underline_var)
+
+settings_font_bold_checkbox.pack()
+settings_font_italic_checkbox.pack()
+settings_font_underline_checkbox.pack()
+
+
+
+
+
+
+
 
 # Передача управления пользователю
 root.mainloop()
