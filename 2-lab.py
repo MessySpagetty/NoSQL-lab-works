057fa4d2c0dc7886f0e98a8cf7a69b388ec7fc0a
MessySpagetty <115343310+MessySpagetty@users.noreply.github.com>
2024-11-28 23:53:01 +0300

    Интерфейс в целом завершён. Надо подключить к бд

--- 2-lab.py
+++ 2-lab.py
@@ -28,17 +28,21 @@ curr_judge = tk.StringVar()
 curr_sportsman = tk.StringVar()
 given_score = tk.StringVar()
 
+# Список судей и спортсменов соответственно
+judges = ("Mr. Red", "Mr. Green", "Mr. Blue")
+sportsmans = ("Faster", "Higher", "Stronger")
+
 # Выбор судьи
 judge_lbl = tk.Label(root, text="Судья:").pack(pady=10)
 
-judge_combo = ttk.Combobox(root, values=list(user_settings_local.keys()), textvariable=curr_judge, state="readonly")
+judge_combo = ttk.Combobox(root, values=judges, textvariable=curr_judge, state="readonly")
 judge_combo.current(0)
 judge_combo.pack()
 
 # Выбор спортсмена
 sportsman_lbl = tk.Label(root, text="Спортсмен:").pack(pady=10)
 
-sportsman_combo = ttk.Combobox(root, values=list(user_settings_local.keys()), textvariable=curr_sportsman, state="readonly")
+sportsman_combo = ttk.Combobox(root, values=sportsmans, textvariable=curr_sportsman, state="readonly")
 sportsman_combo.current(0)
 sportsman_combo.pack()
 
@@ -46,6 +50,21 @@ sportsman_combo.pack()
 sportsman_lbl = tk.Label(root, text="Выставить баллы:").pack(pady=10)
 
 score_entry = tk.Entry(root, textvariable=given_score)
+score_entry.pack()
+
+# Сохранение выставленного балла
+save_score = tk.Button(root, text="Сохранить")
+save_score.pack(pady=10)
+
+# Рейтинг-лист
+columns = ("Surname", "Score")
+rating_tree = ttk.Treeview(root, columns=columns, show="headings")
+rating_tree.heading("Surname", text="Фамилия")
+rating_tree.heading("Score", text="Сумма баллов")
+
+for sp in sportsmans:
+    rating_tree.insert("", "end", values=(sp, 0))
+rating_tree.pack(pady=10)
 
 # Передача управления пользователю
 root.mainloop()
