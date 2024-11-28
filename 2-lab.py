7539fb3596d4f3bfe76fb09fd46d8eef2694bb72
MessySpagetty <115343310+MessySpagetty@users.noreply.github.com>
2024-11-28 22:31:14 +0300

    Сделана простейшая реализация окна. Осталось добавить кнопку.

--- 2-lab.py
+++ 2-lab.py
@@ -19,5 +19,33 @@ MY_PREFIX = "poskitt_22304_"
 root = tk.Tk()
 root.title("Монитор спортивных соревнований")
 
+SCR_WIDTH = root.winfo_screenwidth() 
+SCR_HEIGHT = root.winfo_screenheight()
+root.geometry(f"{SCR_WIDTH}x{SCR_HEIGHT}")
+
+# Переменные для динамичского изменения настроек текста
+curr_judge = tk.StringVar()
+curr_sportsman = tk.StringVar()
+given_score = tk.StringVar()
+
+# Выбор судьи
+judge_lbl = tk.Label(root, text="Судья:").pack(pady=10)
+
+judge_combo = ttk.Combobox(root, values=list(user_settings_local.keys()), textvariable=curr_judge, state="readonly")
+judge_combo.current(0)
+judge_combo.pack()
+
+# Выбор спортсмена
+sportsman_lbl = tk.Label(root, text="Спортсмен:").pack(pady=10)
+
+sportsman_combo = ttk.Combobox(root, values=list(user_settings_local.keys()), textvariable=curr_sportsman, state="readonly")
+sportsman_combo.current(0)
+sportsman_combo.pack()
+
+# Выставление баллов
+sportsman_lbl = tk.Label(root, text="Выставить баллы:").pack(pady=10)
+
+score_entry = tk.Entry(root, textvariable=given_score)
+
 # Передача управления пользователю
 root.mainloop()
